d40ef65d4587e0d4e4c05f1fb7e64fdb4c15f9ca
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2026-06-25 21:37:06 +0600

    Register Notes and EmployeeNotes components in the dependency injection graph.
    - Add `NotesRepository` and `EmployeeNotesRepository` as singletons to the `AppModule`.
    - Register `NotesViewModel` and `EmployeeNotesViewModel` in the Koin module.

--- app/src/main/java/com/appriyo/repairmanager/di/AppModule.kt
+++ app/src/main/java/com/appriyo/repairmanager/di/AppModule.kt
@@ -1,13 +1,17 @@
 package com.appriyo.repairmanager.di
 
 import com.appriyo.repairmanager.data.repository.AuthRepository
+import com.appriyo.repairmanager.data.repository.EmployeeNotesRepository
+import com.appriyo.repairmanager.data.repository.NotesRepository
 import com.appriyo.repairmanager.data.repository.RepairRepository
 import com.appriyo.repairmanager.presentation.viewmodel.AddRepairViewModel
 import com.appriyo.repairmanager.presentation.viewmodel.AuthViewModel
 import com.appriyo.repairmanager.presentation.viewmodel.CustomerDetailsViewModel
 import com.appriyo.repairmanager.presentation.viewmodel.CustomerListViewModel
 import com.appriyo.repairmanager.presentation.viewmodel.EditRepairViewModel
+import com.appriyo.repairmanager.presentation.viewmodel.EmployeeNotesViewModel
 import com.appriyo.repairmanager.presentation.viewmodel.MainViewModel
+import com.appriyo.repairmanager.presentation.viewmodel.NotesViewModel
 import com.google.firebase.firestore.FirebaseFirestore
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
@@ -19,6 +23,8 @@ val appModule = module {
     // Repositories
     single { AuthRepository(get()) }
     single { RepairRepository(get()) }
+    single { NotesRepository(get()) }
+    single { EmployeeNotesRepository(get()) }
 
     // ViewModels
     viewModel { MainViewModel(get()) }
@@ -27,4 +33,6 @@ val appModule = module {
     viewModel { CustomerListViewModel(get()) }
     viewModel { CustomerDetailsViewModel(get()) }
     viewModel { EditRepairViewModel(get()) }
+    viewModel { NotesViewModel(get(), get()) }
+    viewModel { EmployeeNotesViewModel(get(), get()) }
 }

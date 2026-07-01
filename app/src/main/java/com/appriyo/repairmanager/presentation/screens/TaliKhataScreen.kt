c95702bc66872a56c5c0619ed2fbbe331d07a125
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2026-07-01 21:02:20 +0600

    Refactor and change the package name

--- app/src/main/java/com/appriyo/repairmanager/presentation/screens/TaliKhataScreen.kt
+++ app/src/main/java/com/appriyo/repairmanager/presentation/screens/TaliKhataScreen.kt
@@ -1,4 +1,4 @@
-package com.appriyo.repairmanager.presentation.screens.talikhata
+package com.appriyo.repairmanager.presentation.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
